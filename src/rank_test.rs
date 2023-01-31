3cbf788034be1e0e89da2facb7fd0f249060e830
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-01-31 12:34:26 -0800

    added testing on rank test

--- src/rank_test.rs
+++ src/rank_test.rs
@@ -2,6 +2,8 @@ use crate::{mwu::mann_whitney_u, utils::select_ranks};
 use ndarray::{Array1, Axis};
 use ndarray_rand::{rand_distr::Uniform, RandomExt};
 
+/// Performs a rank test for each gene in the dataset.
+/// Returns a tuple of vectors containing the U and p-values for each gene.
 pub fn rank_test(
     n_genes: usize,
     ntc_index: usize,
@@ -16,6 +18,8 @@ pub fn rank_test(
         .unzip()
 }
 
+/// Performs a rank test for pseudo genes created from the non-targeting controls
+/// Returns a tuple of vectors containing the U and p-values for each pseudogene.
 pub fn pseudo_rank_test(
     n_pseudo: usize,
     s_pseudo: usize,
@@ -34,4 +38,32 @@ pub fn pseudo_rank_test(
         })
         .map(|(in_group, out_group)| mann_whitney_u(&in_group, &out_group))
         .unzip()
+}
+
+#[cfg(test)]
+mod testing {
+    use super::rank_test;
+    use ndarray::array;
+
+    
+    #[test]
+    fn test_rank_test() {
+        let n_genes = 2;
+        let ntc_index = 0;
+        let encoding = vec![0, 1, 2, 0, 1, 2, 0, 1, 2, 0];
+        let pvalues = array![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.0];
+        let ntc_values = array![0.7, 0.8, 0.9, 0.7, 0.8];
+        let (u, p) = rank_test(n_genes, ntc_index, &encoding, &pvalues, &ntc_values);
+        assert_eq!(u, vec![3.0, 4.5]);
+        assert_eq!(p, vec![0.08985624744357722, 0.18554668477815348]);
+    }
+
+    #[test]
+    fn test_pseudo_rank_test() {
+        let n_pseudo = 2;
+        let s_pseudo = 3;
+        let ntc_values = array![0.7, 0.7, 0.7, 0.7, 0.7, 0.7];
+        let (_u, p) = super::pseudo_rank_test(n_pseudo, s_pseudo, &ntc_values);
+        assert_eq!(p, vec![0.5, 0.5]);
+    }
 }
