3fcc63e0474c142e39ed6e854f0cea7591cfbb0f
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-01-31 16:04:05 -0800

    make more of the lib public

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-mod encode;
-mod fdr;
 pub mod inc;
-mod mwu;
-mod rank_test;
-mod result;
-mod utils;
+pub mod encode;
+pub mod fdr;
+pub mod mwu;
+pub mod rank_test;
+pub mod result;
+pub mod utils;
+
+pub use inc::Inc;
+pub use encode::EncodeIndex;
+pub use fdr::{FdrResult, Fdr};
+pub use mwu::mann_whitney_u;
+pub use result::IncResult;
