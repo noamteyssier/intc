4a40cb2e38a403bba71cb59fca4275683e6ad149
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-02-01 15:16:50 -0800

    added continuity argument to rank tests

--- src/rank_test.rs
+++ src/rank_test.rs
@@ -10,11 +10,12 @@ pub fn rank_test(
     encoding: &[usize],
     pvalues: &Array1<f64>,
     ntc_values: &Array1<f64>,
+    continuity: bool,
 ) -> (Vec<f64>, Vec<f64>) {
     (0..=n_genes)
         .filter(|x| *x != ntc_index)
         .map(|x| select_ranks(x, encoding, pvalues))
-        .map(|x| mann_whitney_u(&x, ntc_values))
+        .map(|x| mann_whitney_u(&x, ntc_values, continuity))
         .unzip()
 }
 
@@ -24,6 +25,7 @@ pub fn pseudo_rank_test(
     n_pseudo: usize,
     s_pseudo: usize,
     ntc_values: &Array1<f64>,
+    continuity: bool,
 ) -> (Vec<f64>, Vec<f64>) {
     (0..n_pseudo)
         .map(|_| Array1::random(s_pseudo, Uniform::new(0, ntc_values.len())))
@@ -36,7 +38,7 @@ pub fn pseudo_rank_test(
             let out_group = ntc_values.select(Axis(0), &out_mask);
             (in_group, out_group)
         })
-        .map(|(in_group, out_group)| mann_whitney_u(&in_group, &out_group))
+        .map(|(in_group, out_group)| mann_whitney_u(&in_group, &out_group, continuity))
         .unzip()
 }
 
@@ -52,7 +54,7 @@ mod testing {
         let encoding = vec![0, 1, 2, 0, 1, 2, 0, 1, 2, 0];
         let pvalues = array![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.0];
         let ntc_values = array![0.7, 0.8, 0.9, 0.7, 0.8];
-        let (u, p) = rank_test(n_genes, ntc_index, &encoding, &pvalues, &ntc_values);
+        let (u, p) = rank_test(n_genes, ntc_index, &encoding, &pvalues, &ntc_values, false);
         assert_eq!(u, vec![3.0, 4.5]);
         assert_eq!(p, vec![0.08985624744357722, 0.18554668477815348]);
     }
@@ -62,7 +64,7 @@ mod testing {
         let n_pseudo = 2;
         let s_pseudo = 3;
         let ntc_values = array![0.7, 0.7, 0.7, 0.7, 0.7, 0.7];
-        let (_u, p) = super::pseudo_rank_test(n_pseudo, s_pseudo, &ntc_values);
+        let (_u, p) = super::pseudo_rank_test(n_pseudo, s_pseudo, &ntc_values, false);
         assert_eq!(p, vec![0.5, 0.5]);
     }
 }
